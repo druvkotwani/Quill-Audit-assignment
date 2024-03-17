a5bc4786501d228054bbebd0e305e991143c8a7d
druvkotwani <druvkotwani12@gmail.com>
2024-03-17 09:37:40 +0530

    Refactor Navbar component and add responsive design

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -3,13 +3,36 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className="flex  justify-between items-center p-4">
-            <Image src={'/images/quillauditsicon.png'} alt="logo" width={41} height={39} />
-            <button className='font-medium text-[12.7px] leading-[17.4px] text-white  text-center shadow-[0px_9.655799865722656px_14.483699798583984px_-2.896739959716797px_#0000001A] py-2 px-3 rounded-[4.39px] bg-[linear-gradient(267.25deg,_#7184FD_20%,_#3F7AF0_50%,_#4F37EE_100%)]'>
-                Request An Audit
-            </button>
-            <button className=''>{hamburger()}</button>
-        </div>
+        <>
+            {/* Mobile devices */}
+            <div className="flex  justify-between items-center p-4 sm:hidden">
+                <Image src={'/images/quillauditsicon.png'} alt="logo" width={41} height={39} />
+                <button className='font-medium text-[12.7px] leading-[17.4px] text-white  text-center shadow-[0px_9.655799865722656px_14.483699798583984px_-2.896739959716797px_#0000001A] py-2 px-3 rounded-[4.39px] bg-[linear-gradient(267.25deg,_#7184FD_20%,_#3F7AF0_50%,_#4F37EE_100%)]'>
+                    Request An Audit
+                </button>
+                <button className=''>{hamburger()}</button>
+            </div>
+
+            {/* Desktop */}
+            <div className='hidden sm:block  xl:px-[8%] py-4'>
+                <div className='flex justify-between items-center border-b-2 pb-4 border-b-[#8a8a8a] border-opacity-15'>
+                    <Image src={'/images/QuillAudits.png'} alt="logo" width={217} height={43} />
+
+                    <ul className='flex items-center justify-center gap-6 leading-[32.13px] text-[19.42px] text-white'>
+                        <li className='cursor-pointer'>Services</li>
+                        <li className='cursor-pointer'>Pricing</li>
+                        <li className='cursor-pointer'>Our Audits</li>
+                        <li className='cursor-pointer'>Tools</li>
+                        <li className='cursor-pointer'>Resources</li>
+                        <li className='cursor-pointer'>Refer-Earn-Secure</li>
+                    </ul>
+
+                    <button className='font-medium leading-[32.13px] text-[19.42px] text-white  text-center shadow-[0px_9.655799865722656px_14.483699798583984px_-2.896739959716797px_#0000001A] py-2 px-3 rounded-[4.39px] bg-[linear-gradient(267.25deg,_#7184FD_20%,_#3F7AF0_50%,_#4F37EE_100%)]'>
+                        Request An Audit
+                    </button>
+                </div>
+            </div>
+        </>
     )
 }
 
